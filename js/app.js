ad53b7144eee396e857be92ad5c54b15cabab3aa
Victoria Fuselier <vic.fuselier@gmail.com>
2023-06-02 16:04:04 -0500

    CSS changes to try to fix horizontal scrolling; JS alert changes

--- js/app.js
+++ js/app.js
@@ -6,8 +6,8 @@ const alertBanner = document.getElementById('alert');
 
 alertBanner.innerHTML = `
     <div class = 'alert-banner'>
-        <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
         <p class= 'alert-banner-close'>x</p>
+        <p class='alert-banner-msg'><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
     </div>
 `;
 
