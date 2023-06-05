8abe7b4881b7e09e82c3e59db26dca03980b075f
Victoria Fuselier <vic.fuselier@gmail.com>
2023-06-04 21:59:37 -0500

    Added autocomplete feature
    Added autocomplete feature as an extra credit requirement. Based it off of W3Schools tutorial. Some styling has affect the search bar's width. Will try to fix tomorrow and maybe ask Sof/Slack moderators for help if I can't identify the culprit

--- js/app.js
+++ js/app.js
@@ -170,4 +170,113 @@ window.addEventListener('beforeprint', () => {
   window.addEventListener('afterprint', () => {
     myChart.resize();
   });
-   
+
+// =================================================
+//              SEARCH BAR AUTOCOMPLETE
+// =================================================  
+
+// let searchBar = document.querySelector('#userField');
+// let searchInput = searchBar.value;
+// searchBar.addEventListener('keydown', (e) => {
+//     if (searchInput === )
+// });
+
+const namesList = [
+    'Victoria Chambers',
+    'Dale Byrd',
+    'Dawn Wood',
+    'Dan Oliver'
+];
+
+function autocomplete(inp, arr) {
+    // the function takes two arguments, the text field element and array of possible autocomplete values
+    var currentFocus;
+    // executes a function when someone types in the search bar
+    inp.addEventListener('input', function(e) {
+        var a, b, i, val = this.value;
+        // close any already open lists of autocompleted values
+        closeAllLists();
+        if (!val) {
+            return false;
+        }
+        currentFocus = -1;
+        // create a div element that will contain the items (values)
+        a = document.createElement('div');
+        a.setAttribute('id', this.id + 'autocomplete-list');
+        a.setAttribute('class', 'autocomplete-items');
+        // append the div element as a child of the autocomplete container...
+        this.parentNode.appendChild(a);
+        // For each item in the array...
+        for (let i = 0; i < arr.length; i++) {
+            //check if item starts with same letters as the search input
+            if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                // create a div element for each matching element
+                b = document.createElement('div');
+                // make matching letters bold
+                b.innerHTML = '<strong>' + arr[i].substr(0, val.length) + '</strong>';
+                b.innerHTML += arr[i].substr(val.length);
+                // insert an input field that will hold current array item's value
+                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                // execute a function when someone clicks on the item value div element
+                b.addEventListener('click', function(e) {
+                    // insert the value for the autocomplete text field
+                    inp.value = this.getElementsByTagName('input')[0].value;
+                    // close list of autocompleted values or any other open lists of autocomplete values
+                    closeAllLists();
+                });
+                a.appendChild(b);
+            }
+        }
+    });
+    // execute a function when user presses a key on the keyboard
+    inp.addEventListener('keydown', function(e) {
+        var x = document.getElementById(this.id + 'autocomplete-list');
+        if (x) x = x.getElementsByTagName('div');
+        if (e.keyCode == 40) {
+            // If the down arrow key is pressed, increase the currentFocus variable
+            currentFocus++;
+            addActive(x);
+        } else if (e.keyCode == 38) {
+            // If the up arrow key is pressed, decrease the currentFocus variable..
+            currentFocus--;
+            // ..and make the current item more visible;
+            addActive(x);
+        } else if (e.keyCode == 13) {
+            // If the enter key is pressed, prevent form from being submitted...
+            e.preventDefault();
+            if (currentFocus > -1) {
+                // ..and simulate a click on the active item
+                if (x) x[currentFocus].click();
+            } 
+        }
+    });
+    function addActive(x) {
+        // a function to classify an item as 'active'
+        if (!x) return false;
+        // start by removing the active class on all items
+        removeActive(x);
+        if (currentFocus >= x.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = (x.length - 1);
+        // add class 'autocomplete-active'
+        x[currentFocus].classList.add('autocomplete-active');
+    }
+    function removeActive(x) {
+        // a function to remove the active class from all autocomplete items
+        for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove('autocomplete-active');
+        }
+    }
+    function closeAllLists(elmnt) {
+        // close all autocomplete lists in the document, except the one passed as an argument
+        var x = document.getElementsByClassName('autocomplete-items');
+        for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != inp) {
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+    }
+    // execute a function when someone clicks in the document
+    document.addEventListener('click', function(e) {
+        closeAllLists(e.target);
+    });
+}
